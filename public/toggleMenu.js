57a66717e8885e1c636f00c9a78822c9647850e1
marinitx <mhiguera@student.42madrid.com>
2024-07-30 18:46:13 +0200

    navigation menu fixed for small devices

--- public/toggleMenu.js
+++ public/toggleMenu.js
@@ -2,9 +2,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('nav-menu');
+    const overlay = document.getElementById('overlay');
   
-    hamburger.addEventListener('click', function () {
-      navMenu.classList.toggle('active');
-    });
+    if (hamburger && navMenu) {
+      hamburger.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+        hamburger.classList.toggle('active');
+        overlay.classList.toggle('active');
+      });
+
+    overlay.addEventListener('click', function () {
+        navMenu.classList.remove('active');
+        hamburger.classList.remove('active');
+        overlay.classList.remove('active');
+      });
+    }
   });
-  
