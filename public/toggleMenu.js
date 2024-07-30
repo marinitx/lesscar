f2e292d71833b81f1647e4fdf5b642aedb18e339
marinitx <mhiguera@student.42madrid.com>
2024-07-30 13:46:21 +0200

    added hamburger menu and fixed more responsive

---
+++ public/toggleMenu.js
@@ -0,0 +1,10 @@
+// public/toggleMenu.js
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+  
+    hamburger.addEventListener('click', function () {
+      navMenu.classList.toggle('active');
+    });
+  });
+  
